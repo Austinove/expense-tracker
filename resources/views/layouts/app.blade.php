e7811c8786aef74fe3da19beb6afde79bda18565
Austinove <bryanovicaustenove@gmail.com>
2020-07-30 09:26:29 +0300

    migrations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,6 +87,41 @@
         $('.top-right').notify({
             message: { text: "Hello its working" }
         }).show();
+
+        $('#registration-form').submit(function (e) {
+        e.preventDefault();
+        var actionUrl = "register";
+        $('#register-btn').html('Submiting...');
+        $("#register-btn").prop('disabled', true);
+        $.ajax({
+            url: actionUrl,
+            type: "post",
+            data: new FormData(this),
+            dataType: 'json',
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            contentType: false,
+            cache: false,
+            processData: false,
+        })
+        .done(response => {
+            $('#register-btn').html('Register User');
+            $("#register-btn").prop('disabled', false);
+            clearInputs();
+            Notification(response.msg, "success");
+        })
+        .fail(error => {
+            if (error.responseJSON.errors.email[0] === "The email has already been taken."){
+                Notification(error.responseJSON.errors.email[0], "warning");
+            }else{
+                Notification("An Error occuired !!!", "warning");
+            }
+            $('#register-btn').html('Register User');
+            $("#register-btn").prop('disabled', false);
+            clearInputs();
+        });
+    });
         
         });
     </script>
